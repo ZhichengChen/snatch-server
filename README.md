6496677a5a5d59946b3c9882aa6b9495635d2b2b
Zhicheng Chen <chenzhicheng@dayuwuxian.com>
2019-06-10 10:51:02 +0800

    feat: add multiple upload feature

--- README.md
+++ README.md
@@ -17,3 +17,33 @@ Once the installation is done, you can run the server:
 node index.js
 ```
 
+## API
+
+GET get all projects
+
+```bash
+/all
+
+[
+  {
+    id: "lucky",
+    pic_wheel: "https://cdn.chenzhicheng.com/images/pic_wheel.png",
+    pic_arrow: "https://cdn.chenzhicheng.com/images/pic_arrow.png",
+    btn_luckydraw: "https://cdn.chenzhicheng.com/images/pic_luckydraw.png",
+    bg_blue: "https://cdn.chenzhicheng.com/images/bg_blue.png"
+  }
+]
+```
+
+POST uplaod images
+
+```bash
+/:projectId
+
+params
+
+{
+  name: "pic_wheel",
+  images: "data:image/png;base64,iVBORw0KGgo=..."
+}
+```
--- index.js
+++ index.js
@@ -1,40 +1,46 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
-const sharp = require('sharp');
+const fs = require('fs');
 
-app.use(bodyParser.urlencoded({extended: false, limit: '50mb'}))
-app.use(bodyParser.json({limit:'50mb'}))
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
 
-const boostSize = {
-	'pic_wheel': [353, 353, 'png'],
-	'pic_arrow': [45, 60, 'png'],
-	'btn_luckydraw': [292, 77, 'png'],
-	'bg_blue': [360, 640, 'jpg']
-}
-
-app.post('/', function(req, res) {
-	const size = boostSize[req.body.name];
-	if (!size) {
-		return res.send('Name invalid!');
-	}
-	let buff = new Buffer(req.body.image, 'base64');
-	let resize = sharp(buff)
-					.resize(size[0], size[1]);
-	if (size[2]=='jpg') {
-		resize = resize.jpeg();
-	} else {
-		resize = resize.png({adaptiveFiltering: true})
-	}
-	resize.toFile(req.body.name+'.'+size[2], (err) => { 
-		if (err) {
-			return res.send(err);
-		} else {
-			return res.send('Upload Success!');
-		}
-	});
+app.get('/', function(req, res) {
+  res.json([
+    {
+      pic_upload_photos_notice_page_popup_png:
+        'https://static.snaptube.in/campaigns/pic_upload_photos_notice_page_popup_pngcjuzltaba0000ey234qkktckr.png',
+      poster_png: 'https://intranet.snappea.com/cjsuer8ia0016yy23w9qdm1vm',
+      pic_my_vote_homepage_prize_2_png:
+        'https://static.snaptube.in/campaigns/pic_my_vote_homepage_prize_2_pngcjuo1u9y5000d4s23m1updw0g.png',
+      bg_new_users_page_png:
+        'https://static.snaptube.in/campaigns/bg_new_users_page_pngcjvw878va0000oo23wfcuzfv0.jpg',
+      rules_voto_png: 'https://intranet.snappea.com/cjsufnjde001qyy23sko093jw'
+    }
+  ]);
 });
 
-app.listen(3000, function(){
-	console.log('ok.');
+app.post('/', function(req, res) {
+  const size = [353, 353, 'png'];
+  if (!size) {
+    return res.send('Name invalid!');
+  }
+  let buff = new Buffer(req.body.image, 'base64');
+
+  fs.writeFile(
+    req.body.name + '.' + size[2],
+    buff,
+    {
+      flags: 'wb'
+    },
+    function(err) {}
+  );
+  res.send('OK');
+});
+
+app.listen(3000, function() {
+  console.log('ok.');
 });
